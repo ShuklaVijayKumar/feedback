fd0b29a72fcfc6d1c73c88e219df3ad8172b8881
ShuklaVijayKumar <shukla.vijay@outlook.com>
2018-07-26 22:45:54 +0100

    Add files via upload

---
+++ Server/config/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+    googleClientID:'900493460607-hfk4df43vlf3t33crch8iqirgskn7va8.apps.googleusercontent.com',
+    googleClientSecret: 'CCDj4dKGCiThtpUI4Li02RhA',
+    mongoURI: 'mongodb://vkumars:Letmein10@ds145881.mlab.com:45881/peepfeed-emaily-dev',
+    cookieKey:'asdfasdfsfasfdasdfsa'
+}
